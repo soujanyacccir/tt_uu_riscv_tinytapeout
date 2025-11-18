7579d1d78e1cf823849179fce5f6d85fd2f13ea3
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-18 13:35:08 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,13 +4,14 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_tiny_riscv(dut):
-    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, unit="ns").start())
+
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 5)
     dut.rst_n.value = 1
 
-    # wait several cycles to see counter increment
-    await ClockCycles(dut.clk, 200)
+    # Wait for the loop to execute a few times
+    await ClockCycles(dut.clk, 500)
 
-   dut._log.info(f"uo_out = {dut.uo_out.value.binstr}")
-
+    # SAFE PRINT
+    dut._log.info(f"uo_out = {dut.uo_out.value.binstr}")
